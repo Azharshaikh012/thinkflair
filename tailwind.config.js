bb555c61fb007825b2d50c3d475fc465a831e822
Azhar Shaikh <sazhar012@gmail.com>
2023-06-19 17:58:01 +0530

    fixed all section spacing, font, form border and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ['./dist/index.html'],
   theme: {
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      smallwide: '0.01em',
+      smallwider: '0.02em',
+      wide: '.025em',
+      midwide: '.03em',
+      wider: '.05em',
+      widest: '.1em',
+      widest: '.25em',
+    },
     extend: {
       colors: {
         'dark-transparent': 'rgba(0, 0, 0, 0.6)',
@@ -24,6 +37,7 @@ module.exports = {
       '12': '48px',
       '13': '52px'
     }
+   
     },
   },
   plugins: [],
