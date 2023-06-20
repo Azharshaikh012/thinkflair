70afa039303fcde6fe28be8c1bd05393706f19de
Azhar Shaikh <sazhar012@gmail.com>
2023-06-20 15:18:55 +0530

    fixed third section, footer and others

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,21 @@ module.exports = {
       widest: '.1em',
       widest: '.25em',
     },
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '4xxl' :'2.5rem',
+      '5mxl' :'2.8rem',
+      '5xl': '3.052rem',
+    },
     extend: {
       colors: {
         'dark-transparent': 'rgba(0, 0, 0, 0.6)',
+        'nero' : '#1A1A1A'
       },
       screens: {
         'xs': '420px',
